769e9b510b1c49d6f403e54fe4c69e1183a5c017
Alexey <4653240-dm38@users.noreply.gitlab.com>
2019-10-29 19:23:50 +0300

    firmware: add support for TP-Link Archer C5 V4.
    Thanks to 123serge123(forum.4pda.ru) for the original patch.

---
+++ trunk/configs/boards/TPLINK/TL_C5-V4/board.h
@@ -0,0 +1,37 @@
+/* TP-LINK ARCHER C2 V1 */
+
+#define BOARD_PID		"TL_C5-V4"
+#define BOARD_NAME		"TL_C5-V4"
+#define BOARD_DESC		"TP-Link Archer C5 Wireless Router"
+#define BOARD_VENDOR_NAME	"TP-LINK Technologies CO., LTD."
+#define BOARD_VENDOR_URL	"http://www.tp-link.com/en"
+#define BOARD_MODEL_URL		"http://www.tp-link.com/en/download/Archer-C5_V4.html"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	13
+#define BOARD_GPIO_BTN_WPS	2
+#define BOARD_GPIO_LED_WPS	1
+#undef  BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+//	72	/* hw led */
+#define  BOARD_GPIO_LED_SW2G	14
+#define  BOARD_GPIO_LED_SW5G	12
+#define  BOARD_GPIO_LED_POWER	42
+#define BOARD_GPIO_LED_LAN	8
+#define BOARD_GPIO_LED_WAN	10
+#undef  BOARD_GPIO_LED_WAN_INVERTED
+#undef  BOARD_GPIO_LED_WAN_PHY
+#undef  BOARD_GPIO_LED_WAN_PHY_INVERTED
+#define BOARD_GPIO_LED_USB	7
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	2
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
---
+++ trunk/configs/boards/TPLINK/TL_C5-V4/board.mk
@@ -0,0 +1,15 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# ARCHERC2  # TP-LINK ARCHER C2    # MT7620  #
+##################################################################
+
+CFLAGS += -DBOARD_ARCHER_C5 -DVENDOR_TPLINK
+BOARD_NUM_USB_PORTS=1
+
+### TP-LINK firmware description ###
+TPLINK_HWID := 0x04DA857C
+TPLINK_HWREV := 0x0C000600
+TPLINK_HWREVADD := 0x04000000
+TPLINK_FLASHLAYOUT := 8Mmtk
+TPLINK_HVERSION=3
