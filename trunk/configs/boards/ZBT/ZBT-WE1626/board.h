78ef51bf3226c6056bcfd8d415b08d29e95d09f8
Alexey <4653240-dm38@users.noreply.gitlab.com>
2020-06-11 00:20:45 +0300

    firmware: Add support ZBT-WE1626

---
+++ trunk/configs/boards/ZBT/ZBT-WE1626/board.h
@@ -0,0 +1,34 @@
+/* Zhibotong ZBT-WE1626 */
+
+#define BOARD_PID		"ZBT-WE1626"
+#define BOARD_NAME		"ZBT-WE1626"
+#define BOARD_DESC		"Zhibotong WE1626 Wireless Router"
+#define BOARD_VENDOR_NAME	"Zhibotong(Shenzhen) Limited"
+#define BOARD_VENDOR_URL	"http://www.zbtlink.com/"
+#define BOARD_MODEL_URL		"http://www.zbtlink.com/products/router/ZBT-WE1626.html"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	1
+#undef BOARD_GPIO_BTN_WPS
+#undef BOARD_GPIO_LED_ALL
+#define	BOARD_GPIO_LED_WIFI	72	/* hw led */
+#undef BOARD_GPIO_LED_SW2G		/* soft led */
+#undef BOARD_GPIO_LED_SW5G		/* soft led */
+#define BOARD_GPIO_LED_POWER	38
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN	
+#define BOARD_GPIO_LED_USB	39
+#define BOARD_GPIO_LED_USB_INVERTED
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC       0
+#define BOARD_NUM_ANT_5G_TX     0
+#define BOARD_NUM_ANT_5G_RX     0
+#define BOARD_NUM_ANT_2G_TX     2
+#define BOARD_NUM_ANT_2G_RX     2
+#define BOARD_NUM_ETH_LEDS      1
+#undef BOARD_ETH_LED_SWAP
+#define BOARD_HAS_EPHY_L1000    0
+#define BOARD_HAS_EPHY_W1000    0
