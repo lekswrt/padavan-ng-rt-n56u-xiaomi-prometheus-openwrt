ce09fa3d7d37a802a7409bbadf2fedf0bbedcfda
Alexey <4653240-dm38@users.noreply.gitlab.com>
2020-10-14 10:20:50 +0300

    firmware: Add support SNR-W4N-M USB mod router (Thanks valeramalko from 4PDA)

---
+++ trunk/configs/boards/SNR/SNR-W4N-M_USB/board.h
@@ -0,0 +1,28 @@
+/* SNR-W4N-M_USB */
+
+#define BOARD_PID		"SNR-W4N-M_USB"
+#define BOARD_NAME		"SNR-W4N-M_USB"
+#define BOARD_DESC		"SNR-W4N-M_USB Wireless Router"
+#define BOARD_VENDOR_NAME	"SNR"
+#define BOARD_VENDOR_URL	"https://nag.ru/"
+#define BOARD_MODEL_URL		"https://wikidevi.wi-cat.ru/NAG_SNR-CPE-W4N_(rev.M)"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	90
+#define BOARD_GPIO_BTN_RESET	1
+#undef  BOARD_GPIO_BTN_WPS	
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI	
+#undef  BOARD_GPIO_LED_POWER	
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN	
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
