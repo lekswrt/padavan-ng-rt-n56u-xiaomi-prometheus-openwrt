8af4ea1a02ad88c05b142290bfb4f22fe1ad91f3
Alexey <4653240-dm38@users.noreply.gitlab.com>
2020-11-09 22:25:09 +0300

    kernel: wireguard: Lost part of "Fix compilation witchout IPv6 (b2b6d5539d1705621773da1802702a1e1f55c169)"

--- trunk/linux-3.4.x/net/wireguard/device.c
+++ trunk/linux-3.4.x/net/wireguard/device.c
@@ -213,8 +213,10 @@ err:
 	++dev->stats.tx_errors;
 	if (skb->protocol == htons(ETH_P_IP))
 		icmp_ndo_send(skb, ICMP_DEST_UNREACH, ICMP_HOST_UNREACH, 0);
+#if IS_ENABLED(CONFIG_IPV6)
 	else if (skb->protocol == htons(ETH_P_IPV6))
 		icmpv6_ndo_send(skb, ICMPV6_DEST_UNREACH, ICMPV6_ADDR_UNREACH, 0);
+#endif
 	kfree_skb(skb);
 	return ret;
 }
