bc80b330645b934011af87e0f1d5bcbc08b0bd89
Alexey <4653240-dm38@users.noreply.gitlab.com>
2020-11-18 00:33:07 +0300

    boards: RT-AC1200GU: Fix params

--- trunk/configs/boards/ASUS/RT-AC1200GU/board.h
+++ trunk/configs/boards/ASUS/RT-AC1200GU/board.h
@@ -4,8 +4,8 @@
 #define BOARD_NAME		"RT-AC1200GU"
 #define BOARD_DESC		"ASUS RT-AC1200GU Wireless Router"
 #define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
-#define BOARD_VENDOR_URL	"http://www.asus.com/"
-#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTN56U_B1/"
+#define BOARD_VENDOR_URL	"https://www.asus.com/"
+#define BOARD_MODEL_URL		"https://www.asus.com/ru/Networking/RT-AC1200GU/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	41
@@ -25,9 +25,8 @@
 #define BOARD_NUM_ANT_5G_RX	2
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
-#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
 #define BOARD_NUM_UPHY_USB3	0
-#define BOARD_USB_PORT_SWAP	0
-
+#define BOARD_USB_PORT_SWAP	1
